f7e1d312033191f56a16e7303484a0e23ab06be1
donkadoom <borkar.21@buckeyemail.osu.edu>
2025-03-19 14:48:58 -0400

    Finished Lab

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+import Orders from './Orders/Orders';
+import OrderDetail from './Orders/OrderDetail';
+
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">Revealing Yeti</a>
+          </div>
+          <div className="header-links">
+            <Link to="/">Home</Link>
+            <Link to="/catalog">Catalog</Link>
+            <Link to="/orders">Orders</Link>
+          </div>
+        </header>
+
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+          </ul>
+        </aside>
+
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Products />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path='/order/:id' element={<OrderDetail/>}></Route>
+          </Routes>
+        </main>
+
+        <footer className="footer">
+          &copy; 2025 Revealing Yeti
+        </footer>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
